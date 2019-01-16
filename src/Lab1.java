89164f2193a15ff59adbf30872cb84002103643e
talexander <talexander@dhcp-10-134-199-195.uawifi.arizona.edu>
2019-01-16 08:14:44 -0700

    Print Statement Changed, Author added

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,14 +1,13 @@
 /**
  * 
- * @author YOUR NAME HERE
+ * @author Thomas Alexander
  * 
  * 
  */
 public class Lab1 {
 
 	public static void main(String[] args) {
-		// TODO Change this to "Hello CSC 335, I'm YOURNAME"
-		System.out.println("Hello, World!");
+		System.out.println("Hello CSC 335, I'm Thomas Alexander");
 	}
 
 }
